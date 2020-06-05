8de5809217a0f85ba0c06525caa60e04fc75c65e
Emre Harbutoğlu <emre.harbutoglu@hemenis.com>
2020-06-05 13:16:57 +0300

    improvements

--- db-migrate.py
+++ db-migrate.py
@@ -2,6 +2,20 @@ fileName = 'MainActivity.java'
 
 bindingName = 'Activity' + fileName.split('Activity')[0] + 'Binding'
 
+commentLines = False
+
+def checkForClass(name):
+    lower = False
+    upper = False
+    for letter in name:
+        if not lower:
+            lower = letter.islower()
+        if not upper:
+            upper = letter.isupper()
+
+    return lower and upper
+
+
 with open(fileName, 'r') as codeClass:
     data = codeClass.read()
 
@@ -10,53 +24,52 @@ with open(fileName, 'r') as codeClass:
     expressions = []
     views = []
     viewIDs = []
-    semicolonRowIndexes = []
-    semicolonColumnIndexes = []
-    willBeDeletedIndexes = []
 
     for n, i in enumerate(lines):
-        if ';' in i:
-            semicolonRowIndexes.append(n)
-            semicolonColumnIndexes.append(i.index(";"))
-        splittedVar = i.split(";")
-
-        for e in splittedVar:
-            expressions.append(e)
+        expressions.append(i)
+            
     
     for n, j in enumerate(expressions):
 
-        if "/" in j:
+        if "/*" in j:
+            commentLines = True
+            continue
+
+        if "*/" in j:
+            commentLines = False
+            continue
+
+        if "/" in j or commentLines:
             continue
 
         if "findViewById" in j:
             assignment = j.split("=")
         
-            views.append(assignment[0].strip())
+            view = assignment[0].strip().split(" ")
+
+            if len(view) > 2:
+                for ind, v in enumerate(view):
+                    if checkForClass(v):
+                        views.append(view[ind + 1])
+                        break
+            else:
+                views.append(view[1])
+
+            # views.append(view[1] if len(view) > 1 else view[0])
             
             viewID = assignment[1].strip().split("findViewById")[1].replace(")","").replace("(R.id.","")
 
-            viewIDs.append(viewID)
-
-            willBeDeletedIndexes.append(n)
+            viewIDs.append(viewID.replace(";",""))
 
             expressions[n] = ''
 
     for n, e in enumerate(expressions):
-        for view in views:
-            if ("findViewById" in e) or (view in e and (not '.' in e) and (not ')' in e) and (not '(' in e)):
+        for t, view in enumerate(views):
+            if ("findViewById" in e) or (view in e and ((not '.' in e) and (not ')' in e) and (not '(' in e))):
                 expressions[n] = ''
+            elif view in e and ('.' in e):
+                expressions[n] = expressions[n].replace(view,"binding."+viewIDs[t])
 
-                willBeDeletedIndexes.append(n)
-      
-
-    for m, o in enumerate(semicolonRowIndexes):
-        if n in willBeDeletedIndexes:
-            continue
-        
-        if n == o:
-            e = expressions[o]
-            ind = semicolonColumnIndexes[m]
-            expressions[o] = e[:ind] + ';' + e[ind:]
-
+    
     for i in expressions:
         print(i)
